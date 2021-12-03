b07725017ef816ebeb22c299012197ab820daf8d
theteachr <sop.theteachr@gmail.com>
2021-12-03 18:59:15 +0530

    Prettify output format???

--- chariot.ml
+++ chariot.ml
@@ -1,6 +1,6 @@
 open Base
 
-(* TODO: different types for part one and two *)
+(* XXX: different types for part one and two *)
 
 let ( << ) = Fn.compose
 
@@ -22,7 +22,13 @@ let solvers : (module Solver) array = [|
 let () = Array.iteri solvers ~f:(fun i (module M) ->
   let open M in
   let open Printf in
-  List.iter [solve_one; solve_two] ~f:(
-    Stdio.print_endline
-    << string_of_output
-    << Fn.flip ( @@ ) (read (sprintf "inputs/%02d.txt" (i + 1)))))
+  let open Stdio in
+
+  print_endline (sprintf "Day %02d" (i + 1)); (* FIXME *)
+
+  let input_data = read (sprintf "inputs/%02d.txt" (i + 1)) in
+
+  List.map [solve_one; solve_two] ~f:(Fn.flip ( @@ ) input_data)
+  |> List.iteri ~f:(fun j solution ->
+      print_endline (sprintf "  Part %02d: %s" (j + 1) (string_of_output solution)
+    )))
