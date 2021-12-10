de5a51a81cc7e17e1b98b3244a0b421bc3bde3b5
theteachr <sop.theteachr@gmail.com>
2021-12-11 02:39:40 +0530

    Fix naming: array -> list

--- binary_diagnostic.ml
+++ binary_diagnostic.ml
@@ -17,17 +17,19 @@ let frequencies data =
   let init_counts = init bit_width (fun _ -> 0) in
   fold_left (map2 ( + )) init_counts data
 
-let dec_of_bool_array =
+let dec_of_bool_list =
   let open List in
-  int_of_float
+  Int.of_float
   << snd
   << fold_left (fun (p, v) bit -> (p +. 1., v +. 2. ** p *. bit)) (0., 0.)
   << map Bool.to_float
 
+let flip_bool_list = List.map not
+
 let solve_one data =
   let half = List.length data / 2 in
   let gamma_rate = frequencies data |> List.rev_map (( <= ) half) in
-  let epsilon_rate = List.map not gamma_rate in
-  dec_of_bool_array gamma_rate * dec_of_bool_array epsilon_rate
+  let epsilon_rate = flip_bool_list gamma_rate in
+  dec_of_bool_list gamma_rate * dec_of_bool_list epsilon_rate
 
-let solve_two data = List.length data
+let solve_two _ = 0
