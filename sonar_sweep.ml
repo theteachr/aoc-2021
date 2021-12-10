7089b1e574643710e0dbe3f3b05266411629fd3f
theteachr <sop.theteachr@gmail.com>
2021-12-11 00:53:04 +0530

    Sanitize var names

--- sonar_sweep.ml
+++ sonar_sweep.ml
@@ -1,7 +1,7 @@
 type input = int list
 type output = int
 
-let string_of_output = string_of_int
+let string_of_output = Int.to_string
 
 let ( << ) f g x = x |> g |> f
 
@@ -10,16 +10,15 @@ let read = List.map int_of_string << String.split_on_char '\n'
 let solve_one report =
   let rec aux inc_count = function
     | [] -> inc_count
-    | hd :: (hd' :: _ as tl) when hd < hd' -> aux (inc_count + 1) tl
-    | _ :: tl -> aux inc_count tl
+    | x :: (x' :: _ as xs) when x < x' -> aux (inc_count + 1) xs
+    | _ :: xs -> aux inc_count xs
   in
   aux 0 report
 
 let solve_two report =
   let rec aux inc_count = function
     | [] -> inc_count
-    | a :: (_ :: _ :: b :: _ as tl)
-    when a < b -> aux (inc_count + 1) tl
-    | _ :: tl -> aux inc_count tl
+    | x :: (_ :: _ :: x' :: _ as xs) when x < x' -> aux (inc_count + 1) xs
+    | _ :: xs -> aux inc_count xs
   in
   aux 0 report
