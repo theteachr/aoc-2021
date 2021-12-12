48f24d7dc0c421262c0df365176508ecc64fef44
theteachr <sop.theteachr@gmail.com>
2021-12-12 12:33:55 +0530

    Decompose into reuseable? functions

--- binary_diagnostic.ml
+++ binary_diagnostic.ml
@@ -5,9 +5,10 @@ let string_of_output = Int.to_string
 
 let ( << ) f g x = x |> g |> f
 
+let list_of_string = List.of_seq << String.to_seq
+
 let read =
   let digit_of_char c = int_of_char c - 48 in
-  let list_of_string = List.of_seq << String.to_seq in
   List.(map (map digit_of_char << list_of_string))
   << String.split_on_char '\n'
 
@@ -23,16 +24,24 @@ let dec_of_bool_list =
   << snd
   << fold_left (fun (p, v) bit -> (p +. 1., v +. 2. ** p *. bit)) (0., 0.)
   << map Bool.to_float
+  << rev
 
 let flip_bool_list = List.map not
 
-let gamma_rate data =
+(** Returns a list of booleans representing
+    the gamma rate. *)
+let gamma_rate_bools data =
   let half = List.length data / 2 in
-  frequencies data |> List.rev_map (( <= ) half)
-
-let epsilon_rate = flip_bool_list << gamma_rate
+  frequencies data |> List.map (( <= ) half)
 
 let solve_one data =
-  dec_of_bool_list (gamma_rate data) * dec_of_bool_list (epsilon_rate data)
+  let gr = gamma_rate_bools data in
+  let er = List.map not gr in
+  List.(fold_left ( * ) 1 (map dec_of_bool_list [gr; er]))
+
+let bool_list_of_string = List.map (( = ) '1') << list_of_string
 
 let solve_two data = List.length data
+
+let data = read "00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010"
+let answer = solve_one data
