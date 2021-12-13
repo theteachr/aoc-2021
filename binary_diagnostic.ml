502d1cd3139055ff5764adf14a69c5188f3fadb2
theteachr <sop.theteachr@gmail.com>
2021-12-14 00:46:36 +0530

    Read into a `bool list list`

--- binary_diagnostic.ml
+++ binary_diagnostic.ml
@@ -1,4 +1,4 @@
-type input = int list list
+type input = bool list list
 type output = int
 
 let string_of_output = Int.to_string
@@ -8,15 +8,14 @@ let ( << ) f g x = x |> g |> f
 let list_of_string = List.of_seq << String.to_seq
 
 let read =
-  let digit_of_char c = int_of_char c - 48 in
-  List.(map (map digit_of_char << list_of_string))
+  List.(map (map (( = ) '1') << list_of_string))
   << String.split_on_char '\n'
 
 let frequencies data =
   let open List in
   let bit_width = (length << hd) data in
   let init_counts = init bit_width (fun _ -> 0) in
-  fold_left (map2 ( + )) init_counts data
+  fold_left (map2 ( + )) init_counts (map (map Bool.to_int) data)
 
 let dec_of_bool_list =
   let open List in
@@ -26,13 +25,11 @@ let dec_of_bool_list =
   << map Bool.to_float
   << rev
 
-let flip_bool_list = List.map not
-
 (** Returns a list of booleans representing
     the gamma rate. *)
 let gamma_rate_bools data =
   let half = List.length data / 2 in
-  frequencies data |> List.map (( <= ) half)
+  frequencies data |> List.map (( < ) half)
 
 let solve_one data =
   let gr = gamma_rate_bools data in
@@ -44,4 +41,6 @@ let bool_list_of_string = List.map (( = ) '1') << list_of_string
 let solve_two data = List.length data
 
 let data = read "00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010"
-let answer = solve_one data
+
+let answer_one = solve_one data
+let answer_two = solve_two data
