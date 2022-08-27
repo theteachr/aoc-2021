a38ef6b55db863c18856e0648a2134128b75bbf8
theteachr <sop.theteachr@gmail.com>
2022-08-28 02:17:22 +0530

    Use domain types

--- dive.ml
+++ dive.ml
@@ -1,29 +1,42 @@
 open Base
 open Core
 
-type input = (string * int) list
+type direction =
+  | Forward
+  | Down
+  | Up
+
+type input = (direction * int) list
 type output = int
 
+type parse_error =
+  | InvalidDirection
+
 let string_of_output = Int.to_string
+let direction_of_string = function
+  | "forward" -> Ok Forward
+  | "down" -> Ok Down
+  | "up" -> Ok Up
+  | _ -> Error InvalidDirection
+
 
 let ( << ) = Fn.compose
 
-(* XXX: a type for direction (results in a less number of `failwith`s) *)
 let read path =
   let pair = function
-    | [direction; value] -> (direction, Int.of_string value)
-    | _ -> failwith "unreachable"
+    | [direction; value] -> (direction_of_string direction, Int.of_string value)
+    | _ -> failwith "Invalid line"
   in
   In_channel.read_lines path
   |> List.map ~f:(pair << (String.split ~on:' '))
+  |> List.filter_map ~f:(fun (d, v) -> Result.ok d |> Option.map ~f:(fun d -> (d, v)))
 
 let solve_one input =
   let (x, y) = List.fold input ~init:(0, 0) ~f:(fun (x, y) (direction, value) ->
     match direction with
-    | "forward" -> (x + value, y)
-    | "down" -> (x, y + value)
-    | "up" -> (x, y - value)
-    | _ -> failwith "unreachable"
+    | Forward -> (x + value, y)
+    | Down -> (x, y + value)
+    | Up -> (x, y - value)
   )
   in
   x * y
@@ -31,10 +44,9 @@ let solve_one input =
 let solve_two input =
   let (x, y, _) = List.fold input ~init:(0, 0, 0) ~f:(fun (x, y, z) (direction, value) ->
     match direction with
-    | "forward" -> (x + value, y + z * value, z)
-    | "down" -> (x, y, z + value)
-    | "up" -> (x, y, z - value)
-    | _ -> failwith "unreachable"
+    | Forward -> (x + value, y + z * value, z)
+    | Down -> (x, y, z + value)
+    | Up -> (x, y, z - value)
   )
   in
   x * y
